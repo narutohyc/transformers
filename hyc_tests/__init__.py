792231136814239b2dee36e3410a3a681d2104b6
narutohyc <1832044043@qq.com>
2023-10-19 20:56:40 +0800

    更新

---
+++ hyc_tests/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env Python
+# -- coding: utf-8 --
+
+"""
+@version: v1.0
+@author: huangyc
+@file: __init__.py.py
+@Description: 
+@time: 2023/10/19 20:34
+"""
---
+++ hyc_tests/bert/base_bert.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env Python
+# -- coding: utf-8 --
+
+"""
+@version: v1.0
+@author: huangyc
+@file: base_bert.py
+@Description: https://huggingface.co/docs/transformers/model_doc/bert#transformers.BertModel
+@time: 2023/10/19 20:35
+"""
+
+
+def run():
+    pass
+
+
+if __name__ == '__main__':
+    run()
